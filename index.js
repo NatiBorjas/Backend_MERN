99df3de61762d74f33b2f32d3e0f897aaee2638c
Gear_Lara <natalia.borjas@gmail.com>
2022-08-07 21:56:59 +0200

    Editando peticiones

--- index.js
+++ index.js
@@ -1,49 +1,155 @@
-class Usuario {
-    constructor (nombre, apellido, libros, mascotas) {
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.libros = libros,
-        this.mascotas = mascotas
+const fs = require('fs');
+// SERVIDOR //
+const express = require('express');
+const {Router} = express;
+const app = express();
+const router = Router();
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => {
+    console.log(`Hola, soy tu servidor escuchando en el puerto [ ${server.address().port} ]`)
+});
+server.on("error", error => console.log(`Error en servidor: ${error}`));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api/productos', router);
+app.use('/public', express.static(__dirname + '/public'));
+
+// MANEJO DE ERROR //
+app.use(function(err,req,res,next) {
+    console.log(err.stack);
+    res.status(500).send('Somenthing broke');
+})
+
+
+// CLASE //
+class Contenedor {
+    constructor(archivo){
+        this.archivo = "./files/productos.json";
     }
 
-    getFullName() {
-        console.log(`Bienvenidx! ${this.nombre} ${this.apellido}`)
-    }
-
-    addMascota(mascota) {
-        this.mascotas.push(mascota);
-    }
-
-    countMascotas() {
-        return console.log(`Total de mascotas: ${this.mascotas.length}`)
-    }
-
-    addBook(titulo, autor) {
-        this.libros.push({titulo, autor});
-    }
-
-    getBookNames() {
-        let titulos = [];
-        for (let i = 0; i < this.libros.length; i++) {
-            titulos.push(this.libros[i].titulo)
+    async getAll() {
+        try {
+            const data = await fs.promises.readFile(this.archivo, 'utf-8');
+            let dataParse = JSON.parse(data)
+            return dataParse;
+        } catch (error) {
+            console.log(`Hubo un error con el archivo: ${this.archivo}`);
         }
-        return console.log(titulos)
+    }
+
+    async save(prod) {
+        try {
+            const data = await fs.promises.readFile(this.archivo, 'utf-8');
+            let productos = JSON.parse(data);
+            const indice = productos.map(i => i.id).sort();
+            prod.id = indice[indice.length -1]+1;
+
+                if (!prod.id) {
+                    prod.id = 1;
+                    productos = [{...prod}];
+                    await fs.promises.writeFile(this.archivo, JSON.stringify(productos));
+                    return productos[0].id
+                } else {
+                    productos.push(prod);
+                    await fs.promises.writeFile(this.archivo,JSON.stringify(productos));
+                    
+                    return productos
+                    // console.log([`Se ha agregado el producto ${JSON.stringify(prod.nombre)} con Id n°: ${JSON.stringify(prod.id)}`]);
+                }
+        } catch (error) {
+            console.log("Hubo un error:" + error);
+        }
+    }
+
+    async getById(id) {
+        try {
+            const data = await fs.promises.readFile(this.archivo, 'utf-8');
+            let productos = JSON.parse(data);
+            let dataParse = productos.find(x => x.id == id);
+
+            if(!prod) {
+                console.log(`No se encuentra el objeto con Id ${id}`)
+            } else {
+                return dataParse
+            }
+            
+        } catch (error) {
+            console.log("No pudo realizarse la busqueda por el siguiente error:\n" + error)
+        }
+    }
+
+    async deleteById(id) {
+        try {
+            const data = await fs.promises.readFile(this.archivo, 'utf-8');
+            let productos = JSON.parse(data);
+            let prod = productos.find(x => x.id == id);
+
+            if(!prod) {
+                console.log(`No se encuentra el objeto con Id ${id}`)
+            } else {
+                productos = productos.filter(i => i.id !== prod.id);
+                await fs.promises.writeFile(this.archivo, JSON.stringify(productos));
+                console.log(`Se ha eliminado el objeto con Id ${id}`);
+            }
+
+        } catch (error) {
+            console.log("Hubo un error al intentar eliminar el objeto:\n" + error)
+        }
+    }
+
+    async getById(id) {
+        try {
+            const data = await fs.promises.readFile(this.archivo, 'utf-8');
+            let productos = JSON.parse(data);
+            let prod = productos.find(x => x.id == id);
+
+            if(!prod) {
+                console.log(`No se encuentra el producto con ID: ${id}`)
+            } else {
+                return prod;
+            }
+        } catch (error) {
+            console.log("No pudo realizarse la busqueda por el siguiente error:\n" + error)
+        }
     }
 }
 
-const user1 = new Usuario ("Natalia", "Borjas", [{titulo: "El señor de los anillos", autor: "J. R. R. Tolkien"}, {titulo: "American gods", autor: "Neil Gaiman"}], ["gato", "perro"])
+const productos = new Contenedor('productos');
 
-//EJECUCIÓN:
-user1.getFullName();
-console.log("Mascotas existentes:");
-console.log(user1.mascotas);
-user1.countMascotas();
-user1.addMascota("loro");
-console.log(user1.mascotas);
-user1.countMascotas();
-console.log("Libros en stock:");
-console.log(user1.libros);
-user1.addBook("One Piece" , "Eichiro Oda");
-console.log(user1.libros);
-console.log("Títulos:");
-user1.getBookNames();
+// PETICIONES // 
+router.get('/', async (req, res) => {
+    let productosAll = await productos.getAll();
+        if (!productosAll) {
+            res.json({error: "Hubo un error en/con el archivo."});
+        } else {
+            res.json(productosAll);
+        }
+    }
+);
+
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    const encontrado = await productos.getById();
+    console.log(encontrado)
+    if (encontrado == undefined) {
+        res.json({error: "Producto no encontrado"});
+    } else {
+        res.json(encontrado)
+    }
+
+    }
+);
+
+router.post('/', async (req,res) => {
+
+})
+
+router.put('/:id', async (req,res) => {
+    
+})
+
+router.delete('/:id', async (req,res) => {
+    
+})
