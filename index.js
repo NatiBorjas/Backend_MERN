6b45cfe70567916264d6a96a054717652b19ccfe
Gear_Lara <natalia.borjas@gmail.com>
2022-08-19 20:51:45 +0200

    EJS agregando css

--- index.js
+++ index.js
@@ -161,34 +161,6 @@ app.get('/formulario', (req,res)=> {
 // METODO SAVE//
 router.post('/', async (req,res) => {
     const agregado = req.body;
-    console.log(agregado)
     await productos.save(agregado);
     res.render('pages/productoAgregado', {titulo: "Producto agregado exitosamente", producto: agregado});
-})
-
-// ACTUALIZAR UN PRODUCTO //
-router.put('/:id', async (req,res) => {
-    const {id} = req.params
-    const {body} = req;
-
-    if (!id) {
-        res.json({error: "Producto no encontrado"});
-        
-    } else {
-        await productos.update(id, body);
-        res.json({mensaje: "Se ha actualizado correctamente el producto"});
-    }
-
-})
-
-// ELIMINAR UN PRODUCTO //
-router.delete('/:id', async (req,res) => {
-    const {id} = req.params;
-    const encontrado = await productos.deleteById(id);
-
-    if (!encontrado) {
-        res.json({error: "Producto no encontrado"});
-    } else {
-        res.json({mensaje: "Se ha elliminado el producto correctamente."});
-    }
 })
